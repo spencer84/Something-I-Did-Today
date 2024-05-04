9be46565d4a176ea6006a441b7efe434be6b1263
Sam Spencer <sam95@vt.edu>
2024-05-04 18:56:39 +0100

    Adding date feature

--- sidt/src/main.rs
+++ sidt/src/main.rs
@@ -1,14 +1,17 @@
 use std::env;
+use chrono::prelude::*;
 
 fn main(){
     let args: Vec<String> = env::args().collect();
     println!("{:?}",args);
 
-    let action = &args[1];
-
     let text = &args[1..];
 
     let entry = text.join(" ");
+
+    let local_date: DateTime<Local> = Local::now();
+
+    let formatted_date: String = local_date.format("%Y-%m-%d").to_string();
 
     println!("{:?}",entry);
 }
