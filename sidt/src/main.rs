ce50b49626fd94f0bf433baacb449f4832339e03
Sam Spencer <sam95@vt.edu>
2024-05-04 18:42:26 +0100

    Joining text

--- sidt/src/main.rs
+++ sidt/src/main.rs
@@ -2,5 +2,13 @@ use std::env;
 
 fn main(){
     let args: Vec<String> = env::args().collect();
-    println!("{:?}",args)
+    println!("{:?}",args);
+
+    let action = &args[1];
+
+    let text = &args[1..];
+
+    let entry = text.join(" ");
+
+    println!("{:?}",entry);
 }
