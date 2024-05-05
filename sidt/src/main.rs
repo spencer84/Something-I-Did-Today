b29360204cfc754f416956f90f7cf44819b7b299
Sam Spencer <sam95@vt.edu>
2024-05-05 12:23:33 +0100

    Trying to add data to parquet

--- sidt/src/main.rs
+++ sidt/src/main.rs
@@ -21,6 +21,8 @@ fn main(){
 
     let formatted_date: String = local_date.format("%Y-%m-%d").to_string();
 
+    let row: Vec<(String, String)> = (formatted_date,entry);
+
     // Write to parquet
 
     let path = "data/journal.parquet";
@@ -33,6 +35,12 @@ fn main(){
 ";
 let schema = Arc::new(parse_message_type(message_type).unwrap());
 let file = fs::File::create(&path).unwrap();
-
+let mut writer = SerializedFileWriter::new(file, schema, Default::default(), row).unwrap();
+let mut row_group_writer = writer.next_row_group().unwrap();
+while let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
+    col_writer.close().unwrap()
+}
+row_group_writer.close().unwrap();
+writer.close().unwrap();
     println!("{:?}",formatted_date + " " + &entry);
 }
