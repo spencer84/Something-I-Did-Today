8dbdd5b3b8df97a15ec3d84a19490a697fd8328a
Sam Spencer <sam95@vt.edu>
2024-05-11 17:24:22 +0100

    Writing data to a text file

--- sidt/src/main.rs
+++ sidt/src/main.rs
@@ -1,46 +1,25 @@
 use std::env;
 use chrono::prelude::*;
-use std::{fs, path::Path, sync::Arc};
-use parquet::{
-    file::{
-        properties::WriterProperties,
-        writer::SerializedFileWriter,
-    },
-    schema::parser::parse_message_type,
-};
+use std::fs;
 
 fn main(){
     let args: Vec<String> = env::args().collect();
     println!("{:?}",args);
 
-    let text = &args[1..];
+    let text: &[String] = &args[1..];
 
-    let entry = text.join(" ");
+    let entry: String = text.join(" ");
 
     let local_date: DateTime<Local> = Local::now();
 
     let formatted_date: String = local_date.format("%Y-%m-%d").to_string();
 
-    let row: Vec<(String, String)> = (formatted_date,entry);
+    let dated_entry: String =  formatted_date + " " + &entry;
 
-    // Write to parquet
+    // Write to text file
 
-    let path = "data/journal.parquet";
+    let path = "journal.txt";
+    fs::write(path, &dated_entry);
 
-    let message_type = "
-  message schema {
-    REQUIRED DateTime date;
-    OPTIONAL String text;
-  }
-";
-let schema = Arc::new(parse_message_type(message_type).unwrap());
-let file = fs::File::create(&path).unwrap();
-let mut writer = SerializedFileWriter::new(file, schema, Default::default(), row).unwrap();
-let mut row_group_writer = writer.next_row_group().unwrap();
-while let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
-    col_writer.close().unwrap()
-}
-row_group_writer.close().unwrap();
-writer.close().unwrap();
-    println!("{:?}",formatted_date + " " + &entry);
+    println!("{:?}",&dated_entry);
 }
