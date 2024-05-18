902dd33985ab43bb3b98b62ba7c3dcb2f31178b7
Sam Spencer <sam95@vt.edu>
2024-05-18 22:15:15 +0100

    Trying to read from text file

--- sidt/src/main.rs
+++ sidt/src/main.rs
@@ -1,6 +1,6 @@
-use std::{env, fs::OpenOptions, io::Write};
+use std::{env, fs::{OpenOptions,File}, io::{self, Result, Lines, BufRead, BufReader, Write}};
 use chrono::prelude::*;
-use std::fs;
+use std::path::Path;
 
 fn main(){
     let args: Vec<String> = env::args().collect();
@@ -26,9 +26,32 @@ fn main(){
     .open(path)
     .unwrap();
 
+    let buf = BufReader::new(&file);
+
+    
+    let Ok(lines) = read_lines(&path)
+    else {
+        println!("Cannot read file!");
+        panic!()
+    };
+
+    let last_entry: Vec<_> = buf.lines().collect();
+
+    for line in lines{
+        println!("{:?}",line);
+    }
+
+    
     // Write to text file
-    file.write_all(&dated_entry.as_bytes());
-    //fs::write(path, );
+    file.write_all(&dated_entry.as_bytes()).expect("Could not write.");
+
+    // How to handle if two entries on the same day?
 
     println!("{:?}",&dated_entry);
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
