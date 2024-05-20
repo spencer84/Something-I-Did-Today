58a1ce5a24c354f6a0a7293ea1bef357fd49236e
Sam Spencer <sam95@vt.edu>
2024-05-20 22:56:23 +0100

    Retrieve previous entry

--- sidt/src/main.rs
+++ sidt/src/main.rs
@@ -14,7 +14,7 @@ fn main(){
 
     let formatted_date: String = local_date.format("%Y-%m-%d").to_string();
 
-    let dated_entry: String =  formatted_date + " " + &entry+"\n";
+    let dated_entry: String =  formatted_date.clone() + " " + &entry+"\n";
 
     let path: &str = "journal.txt";
 
@@ -36,20 +36,40 @@ fn main(){
     };
 
 
-    // print out all lines
-    for line in lines{
-        println!("{:?}",line);
+    // Print out all lines
+    // TODO: Add a command for reading all lines
+
+    // for line in lines{
+    //     println!("{:?}",line.unwrap());
+    // }
+    let mut last_line: String = lines.last().unwrap().unwrap();
+    println!("{:?}",&last_line);
+
+    let last_date: String = last_line.split(" ").take(1).collect();
+    println!("{:?}",&last_date);
+
+    // How to handle if two entries on the same day?
+    // If last_date and formatted_date match, then get the previous entry and add to it (remove the new line)
+
+    if last_date == formatted_date {
+        // Get previous entry
+        let mut previous_entry = last_line.split(" ").next().unwrap();
+        println!("{:?}",&previous_entry);
+        // Add latest entry
+        &previous_entry.strip_suffix("\n") + " " + &entry;
+    } else{
+        // Write to text file
+        file.write_all(&dated_entry.as_bytes()).expect("Could not write.");
     }
 
     
-    // Write to text file
-    file.write_all(&dated_entry.as_bytes()).expect("Could not write.");
 
-    // How to handle if two entries on the same day?
+
 
     println!("{:?}",&dated_entry);
 }
 
+// Quick function to read all lines from a file
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
